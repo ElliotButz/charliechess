1bc9afe771488a99c479c5c09381fea5093bad80
unknown <elliot.butz@etu.umontpellier.fr>
2025-07-09 18:55:43 +0200

    as_direction() method for Color

--- src/position/color.rs
+++ src/position/color.rs
@@ -1,2 +1,11 @@
 #[derive(Clone, Copy)]
 pub enum Color {White=1,Black=-1}
+
+impl Color {
+    pub fn as_direction(&self) -> i8 {
+        match self {
+            Color::White => 1i8,
+            Color::Black => -1i8
+        }}
+
+}
