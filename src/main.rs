09408ebfe3f35e2d34f80bb4e2d303446795cbe8
unknown <elliot.butz@etu.umontpellier.fr>
2025-07-11 23:14:06 +0200

    Starting to enumerate legal pieces moves.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 extern crate num_derive;
 
 pub mod position;
+
 use crate::position::{
     board::Board,
     coordinates::{
-        Coords, Row::*, Column::*}
+        Coords, Row::*, Column::*},
+    color::{
+        Color::{
+            White, Black}
+    },
+    moves::pawn_moves
 };
 fn main() {
     let mut board = Board::at_start_state();
     board.terminal_display();
     board.move_piece(&coords!(E,R2), &coords!(E,R4));
     board.terminal_display();
+    let possibles = pawn_moves::pawn_reachable_squares(&board, &coords!(E,R2), &White);
 }
--- src/position/moves/pawn_moves.rs
+++ src/position/moves/pawn_moves.rs
@@ -1,10 +1,10 @@
 use crate::{coords, piece};
 use crate::position::color::{Color,Color::{White, Black}};
-use crate::position::coordinates::{Coords, Row, Row::*, Column, Column::*, CoordsVec};
+use crate::position::coordinates::{Coords, Row, Row::*, Column, Column::*, CoordsVec,IdxCoordsVec, ExcludeAbsurds};
 use crate::position::pieces::{Piece, PieceKind, PieceKind::{Pawn, Knight, Bishop, Tower, Queen, King}};
-use crate::position::board::BoardMap;
+use crate::position::board::Board;
 
-fn pawn_reachable_squares(boardmap:&BoardMap, start:&Coords, color:&Color) -> CoordsVec{
+pub fn pawn_reachable_squares(board:&Board, start:&Coords, color:&Color) -> CoordsVec{
     let direction: i8 = color.as_direction();
     let mut in_reach = CoordsVec::with_capacity(4);
     
@@ -13,10 +13,13 @@ fn pawn_reachable_squares(boardmap:&BoardMap, start:&Coords, color:&Color) -> Co
         _ => false
     };
 
-    let (start_row_idx, start_col_idx) = start.to_colrow_idx();
+    let (start_row_idx, start_col_idx) = start.to_colidx_rowidx();
     if is_at_start {
-        let two_forward = Coords::from_colrow_idx(start_col_idx+2*direction, start_col_idx);
+        let two_forward = Coords::from_colidx_rowidx(start_row_idx+2*direction, start_col_idx);
+        in_reach.push(two_forward)
     }
     
+    in_reach.exclude_absurds(board, color);
+
     in_reach
 } 
