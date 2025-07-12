3f2ac4a7a2bbbf4ba36bb87cca7e23d54af5506a
unknown <elliot.butz@etu.umontpellier.fr>
2025-07-12 10:40:38 +0200

    Method coup_zero() and is_pawn_double_step

--- src/position/coup.rs
+++ src/position/coup.rs
@@ -1,5 +1,7 @@
-use crate::position::coordinates::Coords;
-use crate::position::pieces::Piece;
+use crate::position::coordinates::{Column, Row, Coords};
+use crate::position::pieces::{Piece,PieceKind};
+use crate::position::color::Color;
+use crate::coords;
 
 #[derive(Clone, Copy)]
 pub struct Coup {
@@ -8,6 +10,24 @@ pub struct Coup {
     pub piece:  Piece,
     pub taken:  Option<Piece>,
     pub checks: bool
+}
+
+impl Coup {
+
+    pub fn is_pawn_double_step(&self) -> bool {
+        self.piece.kind == PieceKind::Pawn &&
+        (self.start.to_colrow_idx().row-self.end.to_colrow_idx().row).abs() == 2
+    }
+
+    pub fn coup_zero() -> Self {
+        Self {
+            start:  coords!(Column::C, Row::R3),
+            end:    coords!(Column::B, Row::R1),
+            piece:  Piece {color: Color::White, kind: PieceKind::Knight},
+            taken:  None,
+            checks: false
+        }
+    }
 }
 
 #[derive(Clone, Copy)]
