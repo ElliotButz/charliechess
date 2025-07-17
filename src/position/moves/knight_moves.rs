757eba57cab9aeb72dafaf525c2e0c5528a5fdb2
unknown <elliot.butz@etu.umontpellier.fr>
2025-07-17 07:54:43 +0200

    Corrected yesterday's bug

--- src/position/moves/knight_moves.rs
+++ src/position/moves/knight_moves.rs
@@ -8,9 +8,9 @@ pub fn knight_reachable_squares(board:&Board, start: Square, color:Color) -> (Sq
     // Makes a vector of squares coordinates where a Knight can go, given its start squart, its color, and its board.
     // This function does not take in account the fact that the piece might be pined, since it's more efficient to calculate the reachable 
     // squares of the pieces that are not pinned only.
-
+    let cstart =Coords::from(start);
     let jumps: Vec<(i8, i8)> = vec![(2,-1), (2,1), (-2,-1), (-2,1), (1,2), (-1,2), (1,-2), (-1,-2)];
-    let mut coords_in_reach: CoordsVec = jumps.iter().map(|&jump|Coords::from(start + jump)).collect();
+    let mut coords_in_reach: CoordsVec = jumps.iter().map(|&jump| cstart + jump).collect();
     coords_in_reach.retain(|&target| target.in_board());
     board.targetables_and_stared_pieces(
         coords_in_reach.to_square_vec(),
